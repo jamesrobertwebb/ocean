c0c1a570b7eecaa8330761bd15dc954610834039
james.webb <jamesrobertwebb@gmail.com>
2022-02-24 16:45:01 +0000

    initial commit for ocean

--- nalubale/settings.py
+++ nalubale/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -131,5 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+from .cdn.conf import *
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
